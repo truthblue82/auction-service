8c611d42a7c8a71f08d8b493f8577e038796d48f
truthblue82 <truthblue82@gmail.com>
2020-06-05 16:52:50 +0700

    optimize

--- auction_service/src/handlers/uploadAuctionPicture.js
+++ auction_service/src/handlers/uploadAuctionPicture.js
@@ -4,18 +4,27 @@ import middy from '@middy/core';
 import httpErrorHandler from '@middy/http-error-handler';
 import createError from 'http-errors';
 import { setAuctionPictureUrl } from '../lib/setAuctionPictureUrl';
+import validator from '@middy/validator';
+import uploadAuctionPictureSchema from '../lib/schemas/uploadAuctionPictureSchema';
 
 export async function uploadAuctionPicture(event) {
     const { id } = event.pathParameters;
+    const { email } = event.requestContext.authorizer;
     const auction = await getAuctionById(id);
+
+    // Validate auction ownership
+    if(auction.seller !== email ) {
+        throw new createError.Forbidden('You are not the seller of the auction!');
+    }
+
     const base64 = event.body.replace(/^data:image\/\w+;base64,/, '');
     const buffer = Buffer.from(base64, 'base64');
 
-    let updateAuction;
+    let updatedAuction;
 
     try {
         const pictureUrl = await uploadPictureToS3(auction.id + '.jpg', buffer);
-        updateAuction = await setAuctionPictureUrl(auction.id, pictureUrl);
+        updatedAuction = await setAuctionPictureUrl(auction.id, pictureUrl);
     } catch (error) {
         console.error(error);
         throw new createError.InteralServerError(error);
@@ -23,9 +32,11 @@ export async function uploadAuctionPicture(event) {
 
     return {
         statusCode: 200,
-        body: JSON.stringify(updateAuction)
+        body: JSON.stringify(updatedAuction)
     };
     
 }
 
-export const handler = middy(uploadAuctionPicture).use(httpErrorHandler());
+export const handler = middy(uploadAuctionPicture)
+    .use(httpErrorHandler())
+    .user(validator({inputSchema: uploadAuctionPictureSchema }));
