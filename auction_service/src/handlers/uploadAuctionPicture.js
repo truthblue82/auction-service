f71dba820f6211bf9d08c1f74728af895d742475
truthblue82 <truthblue82@gmail.com>
2020-06-05 15:09:08 +0700

    upload picture

--- auction_service/src/handlers/uploadAuctionPicture.js
+++ auction_service/src/handlers/uploadAuctionPicture.js
@@ -1,8 +1,31 @@
+import { getAuctionById } from './getAuction';
+import { uploadPictureToS3 } from '../lib/uploadPictureToS3';
+import middy from '@middy/core';
+import httpErrorHandler from '@middy/http-error-handler';
+import createError from '../lib/uploadPictureToS3';
+import { setAuctionPictureUrl } from '../lib/setAuctionPictureUrl';
+
 export async function uploadAuctionPicture(event) {
+    const { id } = event.pathParameters;
+    const auction = await getAuctionById(id);
+    const base64 = event.body.replace(/^data:image\/\w+;base64,/, '');
+    const buffer = Buffer.from(base64, 'base64');
+
+    let updateAuction;
+
+    try {
+        const pictureUrl = await uploadPictureToS3(auction.id + '.jpg', buffer);
+        updateAuction = await setAuctionPictureUrl(auction.id, pictureUrl);
+    } catch (error) {
+        console.error(error);
+        throw new createError.InteralServerError(error);
+    }
+
     return {
         statusCode: 200,
-        body: JSON.stringify({})
+        body: JSON.stringify(updateAuction)
     };
+    
 }
 
-export const handler = uploadAuctionPicture;
+export const handler = middy(uploadAuctionPicture).use(httpErrorHandler());
